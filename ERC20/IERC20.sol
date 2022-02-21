fb5e9197aa7fb934c30fc74cd80a2a076d3bd7d6
Snerdify <91380693+Snerdify@users.noreply.github.com>
2022-02-21 12:50:06 +0530

    Update IERC20.sol

--- ERC20/IERC20.sol
+++ ERC20/IERC20.sol
@@ -7,14 +7,19 @@ pragma solidity ^0.8.10;
 interface IERC20{
     //total amount of ERC20 tokens- Anyone can view this function and it returns a uint256 value
     function totalSupply() external view returns (uint256); 
+    
     // returns the amount of ERC20 token that that account has-Anyone can view this func and it returns a uint256 value
     function balanceOf(address account ) external view returns (uint256); 
+    
     //Holder of the ERC20 token can call the function transfer to transfer an amount of ERC20 tokens to the recipient
     function transfer (address recipient , uint256 amount) external returns (bool); 
-    //amount of tokens a spender can spend from a token holder or how much the token holder allows the spender to spend.
-    function allowance (address owner , address spender) external view returns (uint256); 
+    
     // if the token holder does not himself wants to transfer the tokens , then he/she can call the function approve,approving the spender to spend some of his tokens
     function approve(address spender , uint256 amount ) external  returns (bool);
+    
+    //amount of tokens a spender can spend from a token holder or how much the token holder allows the spender to spend.
+    function allowance (address owner , address spender) external view returns (uint256); 
+    
     //once the token holder approves the spender to spend the token on his behalf the spender can call the function transferFrom to transfer from the holder to another recipient 
     function transferFrom(address sender,address recipient , uint256 amount) external  returns (bool);
     
