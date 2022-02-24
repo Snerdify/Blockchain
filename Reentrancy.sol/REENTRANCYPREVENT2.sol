9d8441f56560be8d95fb20e7e3817881ac3e38c8
Snerdify <91380693+Snerdify@users.noreply.github.com>
2022-02-24 21:58:35 +0530

    Create REENTRANCYPREVENT2.sol

---
+++ Reentrancy.sol/REENTRANCYPREVENT2.sol
@@ -0,0 +1,13 @@
+//here we use modifiers to prevent a reentrnacy attack
+
+
+contract ReEntrancyGuard {
+    bool internal locked;
+
+    modifier noReentrant() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+}
